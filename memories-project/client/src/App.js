6782418956c5a0b1f4e6859f54e41a84241254c1
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-10 15:17:14 +0300

    Database Setup
    database imizi kuruyoruz ve client a bağlıyoruz

---
+++ memories-project/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+
+    return (
+        <>
+        Selamın aleyküm
+        </>
+    )
+};
+export default App;
--- memories-project/server/index.js
+++ memories-project/server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from "body-parser";
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://alperaykan:1995Alper.@cluster0.m56glgj.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, err => {
+    if(err) throw err;
+    console.log('connected to MongoDB');
+})
+    // .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    // .catch((error) => console.log(`${error} did not connect`));
