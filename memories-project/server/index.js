855fc9f1178430f5625bdfbdc61aedd304cc51ff
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-10 16:03:35 +0300

    Database
    Database connection sorunu halledildi.

--- memories-project/server/index.js
+++ memories-project/server/index.js
@@ -3,18 +3,21 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import postsRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postsRoutes);
 
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://alperaykan:1995Alper.@cluster0.m56glgj.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 mongoose.connect(CONNECTION_URL, err => {
-    if(err) throw err;
-    console.log('connected to MongoDB');
+    if (err) throw err;
+    console.log(`Server Running on Port: http://localhost:${PORT}`)
 })
-    // .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    // .catch((error) => console.log(`${error} did not connect`));
+app.listen(PORT);
