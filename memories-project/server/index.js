483c15c7ed008c38b27cb32423efa1cc1433440e
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-16 18:25:39 +0300

    Port Değişikliği
    Kafaların karışmaması için server portunu 5000 yaptım

--- memories-project/server/index.js
+++ memories-project/server/index.js
@@ -7,14 +7,15 @@ import postsRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postsRoutes);
-
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
+app.use('/posts', postsRoutes);
+
+
 const CONNECTION_URL = 'mongodb+srv://alperaykan:1995Alper.@cluster0.m56glgj.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, err => {
     if (err) throw err;
