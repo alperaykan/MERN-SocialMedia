f6be1c21905d1ed897819aa80cf452617b8e18fe
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-16 17:17:23 +0300

    Ön yüz çalışmaları
    Önyüze form ve postlar için iskelet yapıldı.

--- memories-project/client/src/App.js
+++ memories-project/client/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Container ,AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from './images/memories.jpg';
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+
 
 const App = () => {
-
-
     return (
-        <>
-        Selamın aleyküm
-        </>
+        <Container maxWidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography align='center' variant='h2' >Memories</Typography>
+                <img src={memories} alt='memories' height='60'  />
+            </AppBar>
+
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7} >
+                            <Posts />
+                        </Grid>
+
+                        <Grid item xs={12} sm={4} >
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+
+        </Container>
     )
 };
 export default App;
---
+++ memories-project/client/src/components/Form/Form.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <h2>Form</h2>
+    )
+};
+export default Form;
