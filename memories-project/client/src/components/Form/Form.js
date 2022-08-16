0ecc2fb0a54d1a4c25bca8ad4e786a28b2b1f5b8
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-16 19:10:10 +0300

    Form oluştu
    Form yapısı oluşturuldu

--- memories-project/client/src/components/Form/Form.js
+++ memories-project/client/src/components/Form/Form.js
@@ -1,11 +1,116 @@
-import React from "react";
+import React, {useState} from "react";
 import useStyles from './Form.module.styles';
+import {TextField, Typography, Button, Paper} from "@material-ui/core";
+import FileBase from 'react-file-base64';
 
 const Form = () => {
     const classes = useStyles();
+
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        tags: '',
+        message: '',
+        selectedFile: ''
+    });
+
+    const handleSubmit = () => {
+
+    };
+
+    const clear = () => {
+
+    }
+
     return (
         <>
-            <h2 >Form</h2>
+            <Paper className={classes.paper}>
+                <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+
+                    <Typography variant='h6'>Create a Memory</Typography>
+
+                    <TextField
+                        variant='outlined'
+                        fullWidth
+                        label='Creator'
+                        name='creator'
+                        value={postData.creator}
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setPostData({...postData, creator: e.target.value})
+                        }}
+                    />
+
+                    <TextField
+                        variant='outlined'
+                        fullWidth
+                        label='Title'
+                        name='title'
+                        value={postData.title}
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setPostData({...postData, title: e.target.value})
+                        }}
+                    />
+
+                    <TextField
+                        variant='outlined'
+                        fullWidth
+                        label='Message'
+                        name='message'
+                        value={postData.message}
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setPostData({...postData, message: e.target.value})
+                        }}
+                    />
+
+                    <TextField
+                        variant='outlined'
+                        fullWidth
+                        label='Tags'
+                        name='tags'
+                        inputMode='multiple'
+                        value={postData.tags}
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setPostData({...postData, tags: e.target.value})
+                        }}
+                    />
+
+                    <div className={classes.fileInput}>
+                        <FileBase
+                            type='file'
+                            multiple={false}
+                            onDone={({base64}) => setPostData({...postData, selectedFile: base64})}
+                        />
+                    </div>
+
+                    <Button
+                        className={classes.buttonSubmit}
+                        variant='contained'
+                        color='primary'
+                        size='large'
+                        type='submit'
+                        fullWidth
+                    >
+                        Submit
+                    </Button>
+
+                    <Button
+                        className={classes.buttonSubmit}
+                        variant='contained'
+                        color='secondary'
+                        size='small'
+                        type='clear'
+                        onClick={clear}
+                        fullWidth
+                    >
+                        Clear
+                    </Button>
+
+                </form>
+            </Paper>
         </>
 
     )
